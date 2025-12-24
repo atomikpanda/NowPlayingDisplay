de153b05b209b261ecc197e61ff6df7f2dd8bd78
Bailey Seymour <baileyseymour@protonmail.com>
2025-12-24 08:39:17 -0500

    use gunicorn

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ export XAUTHORITY=/home/pi/.Xauthority
 # Optional: Add a delay to ensure the desktop environment is ready
 
 cd /home/pi/NowPlayingDisplay || exit 1
-/home/pi/.local/bin/uv run now_playing_web.py &
 /home/pi/.local/bin/uv run gunicorn --bind 0.0.0.0:5432 now_playing_web:npapi &
+/home/pi/.local/bin/uv run main.py &
 /home/pi/.local/bin/uv run kiosk.py &
 
 wait
