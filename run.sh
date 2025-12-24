35c3d31fa79037c5499c4398a9d0902ef3580036
Bailey Seymour <baileyseymour@protonmail.com>
2025-12-24 08:21:19 -0500

    run script

--- run.sh
+++ run.sh
@@ -2,9 +2,10 @@
 export DISPLAY=:0
 export XAUTHORITY=/home/pi/.Xauthority
 # Optional: Add a delay to ensure the desktop environment is ready
-sleep 10 
+
 cd /home/pi/NowPlayingDisplay || exit 1
 /home/pi/.local/bin/uv run now_playing_web.py &
+sleep 10
 /home/pi/.local/bin/uv run main.py &
 
 wait
