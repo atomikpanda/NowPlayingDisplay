9e883ddb030b22567e40441cfddd2fee9aff5bca
Bailey Seymour <baileyseymour@protonmail.com>
2025-12-24 16:18:15 -0500

    device

--- audio_capture.py
+++ audio_capture.py
@@ -49,7 +49,7 @@ class MicrophoneCapture:
             print(f"Available audio devices:\n{sd.query_devices()}")
 
         self.stream = sd.InputStream(
-            device="hw:Device,0",
+            device="hw:CARD=Device,DEV=0",
             samplerate=self.sample_rate,
             channels=1,
             dtype=np.int16,
