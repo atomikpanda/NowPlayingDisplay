ba52d670e7d64c3bcc7288b315c0a0d49c6e743d
Bailey Seymour <baileyseymour@protonmail.com>
2025-12-24 08:10:08 -0500

    add service

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+# Optional: Add a delay to ensure the desktop environment is ready
+sleep 10 
 
 uv run now_playing_web.py &
 uv run main.py &
